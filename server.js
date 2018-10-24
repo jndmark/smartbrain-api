5f3834d13dc800c89f69732c4e45bcf508b7493f
Jonas Mark <jndm@live.com>
2018-10-23 22:46:05 -0500

    adding proper port?

--- server.js
+++ server.js
@@ -12,21 +12,19 @@ const image = require('./controllers/image');
 const db = knex({ // Running the knex function on line 5; this just looks cleaner
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'jndm',
-    password : '',
-    database : 'smartbrain'
+  	connectionString : process.env.DATABASE_URL,
+  	ssl: true,
   }
 });
 
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.json());
-app.use(cors());
 
-app.get('/', (req, res) => { res.send(database.users) })
+app.get('/', (req, res) => { res.send('it is working!') })
 
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+app.post('/signin', (req, res) => { signin.handleSignin(db, bcrypt) })
 
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) }) // *THIS WAS	THE FUNCTION THAT FIRST GOT MINIMIZED*
 
@@ -37,8 +35,8 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 
-app.listen(3001, () => {
-	console.log('app is running on port 3001');
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
 })
 
 
